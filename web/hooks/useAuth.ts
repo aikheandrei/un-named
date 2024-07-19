b1cb5523ed24c1b3599cd799e6aae1eedf59238f
aikhe <ikeandrie.ro@gmail.com>
2024-07-19 21:48:26 +0800

    refactor(auth): return signupContext via useSignup hook by signupContext

--- web/hooks/useAuth.ts
+++ web/hooks/useAuth.ts
@@ -2,10 +2,17 @@
 
 import { useContext } from "react";
 
-import { loginContext } from "@/providers/authProvider";
+import { loginContext, signupContext } from "@/providers/authProvider";
 
 export const useLogin = () => {
   const { loginError, loginIsPending, handleLogin } = useContext(loginContext);
 
   return { loginError, loginIsPending, handleLogin };
+};
+
+export const useSignup = () => {
+  const { signupError, signupIsPending, handleSignup } =
+    useContext(signupContext);
+
+  return { signupError, signupIsPending, handleSignup };
 };
