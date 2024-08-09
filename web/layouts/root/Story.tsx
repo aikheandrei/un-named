fdca1d7207b3f4cd7f6eac43f0a4391ae48aacbc
ikeandrie <ikeandrie.ro@gmail.com>
2024-08-10 05:11:34 +0800

    root page layouts skeleton

---
+++ web/layouts/root/Story.tsx
@@ -0,0 +1,13 @@
+export default function Story() {
+  return (
+    <section className="h-[100svh]">
+      <h1>Lorem Ipsum</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis
+        voluptate temporibus repellat eaque tempora perspiciatis tenetur. Ab
+        voluptas quidem earum tenetur doloremque ea. Exercitationem fugiat est
+        explicabo dignissimos sint nesciunt!
+      </p>
+    </section>
+  );
+}
