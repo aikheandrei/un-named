9fcb46aa90aca2093c4288b32bdc1691eac93d2f
ikeandrie <ikeandrie.ro@gmail.com>
2024-09-08 19:24:46 +0800

    kupal na server component with edge runtime ang lala grabe edi convert back to client lahat na ginawa ko it hurts

--- web/components/root/header.tsx
+++ web/components/root/header.tsx
@@ -2,33 +2,16 @@
 
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
-
-import { Button, buttonVariants } from "../ui/button";
-
-import { RouteLinks } from "./utils/route-links";
 
 import { RatingModal } from "./rating-modal";
+import { buttonVariants } from "../ui/button";
+import { RouteLinks } from "./utils/route-links";
 
 export const Header = () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
-  const [modal, toggleModal] = useState(false);
-  let ratings = [];
-
-  const fetchReviews = async () => {
-    // const reviewsData: reviewProps[] = await fetch(
-    //   `${process.env.WEBSITE_URL}/api/reviews`,
-    // ).then((res) => res.json());
-    const session = await fetch(`/api/reviews`).then((res) => res.json());
-
-    if (session) {
-      // ratings = reviewsData;
-      console.log(session);
-    }
-  };
 
   const checkUserSignIn = async () => {
-    const session = await fetch(`/api/auth`).then((res) => res.json());
+    const session = await fetch(`/api/auth`);
     if (session) {
       setIsSignedIn(true);
     } else {
@@ -38,23 +21,18 @@ export const Header = () => {
 
   useEffect(() => {
     checkUserSignIn();
-    fetchReviews();
   });
-
-  const inSignInPage = usePathname() === "/router/overview";
 
   return (
     <header>
       <nav className="fixed left-[50%] h-14 w-[40rem] -translate-x-1/2 gap-2 whitespace-nowrap border border-t-0 font-geistsans backdrop-blur-md">
         <div className="absolute right-[100%] flex flex-col gap-2 pr-8 pt-4">
-          {!inSignInPage && (
-            <Link
-              className={`${buttonVariants({ variant: "default" })} w-24`}
-              href={"/router/overview"}
-            >
-              {isSignedIn ? "Account" : "Sign in"}
-            </Link>
-          )}
+          <Link
+            className={`${buttonVariants({ variant: "default" })} w-24`}
+            href={"/router/overview"}
+          >
+            {isSignedIn ? "Account" : "Sign in"}
+          </Link>
           <RatingModal>Rate!</RatingModal>
           <Link
             className={`${buttonVariants({ variant: "outline" })} w-24`}
