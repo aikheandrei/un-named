372d8bc6cbd5ef5cae3ff9cbc46afde8a08a79de
ikeandrie <ikeandrie.ro@gmail.com>
2024-10-21 00:26:56 +0800

    dynamic header links + route page created

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "@/styles/globals.css";
+import { HeaderLinks } from "@/components/landing/header-links";
 
 const geistSans = localFont({
   src: "../fonts/GeistVF.woff",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <HeaderLinks />
         {children}
       </body>
     </html>
