a839ef564b564b747015127bc63810a6299b0843
ikeandrie <ikeandrie.ro@gmail.com>
2024-11-09 16:45:58 +0800

    root page initial & more structuring

--- web/components/test.tsx
+++
@@ -1,8 +0,0 @@
-export const Test = () => {
-  return (
-    <>
-      {/* <p className="text-center text-[4rem] font-geistmono">sample text</p> */}
-      {/* <a href="/globe.svg" download>file</a> */}
-    </>
-  );
-}
