30be062e4ac6e5913e3f43d9b4d3f7fb2a179b31
ikeandrie <ikeandrie.ro@gmail.com>
2024-11-15 16:49:57 +0800

    organized file structure/connections

---
+++ web/components/test.tsx
@@ -0,0 +1,8 @@
+export const Test = () => {
+  return (
+    <>
+      {/* <p className="text-center text-[4rem] font-geistmono">sample text</p> */}
+      {/* <a href="/globe.svg" download>file</a> */}
+    </>
+  );
+}
--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -4,6 +4,7 @@ export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./layouts/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -11,6 +12,11 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+
+      fontFamily: {
+        geistmono: "var(--font-geist-mono)",
+        geistsans: "var(--font-geist-sans)",
       },
     },
   },
