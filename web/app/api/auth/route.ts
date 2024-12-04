a7a0ebfa639ac025e916b097a1b1fb1014a8ea96
aikhe <ikeandrie.ro@gmail.com>
2024-12-04 18:50:14 +0800

    return user session instead of boolean in auth route for accessing user data (#36)
    * session check if auth + route handler for sessions
    
    * return user session instead of boolean in auth route for accessing user data

--- web/app/api/auth/route.ts
+++ web/app/api/auth/route.ts
@@ -2,18 +2,11 @@ import { NextResponse } from "next/server";
 import { auth } from "./auth";
 
 export async function GET() {
-  const session = await auth();
-  let res;
-
   try {
-    if (!session?.user) {
-      res = { signedIn: false };
-    } else {
-      res = { signedIn: true };
-    }
+    const session = await auth();
 
-    console.log(res);
-    return NextResponse.json(res);
+    console.log(session);
+    return NextResponse.json(session);
   } catch (error) {
     NextResponse.json(
       { error: error instanceof Error ? error.message : "Unknown error" },
