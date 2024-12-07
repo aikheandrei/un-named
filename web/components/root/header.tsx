100207911a769fb8026a50ac9ec8a084746932fa
ikeandrie <ikeandrie.ro@gmail.com>
2024-12-08 01:18:10 +0800

    clean header nav

--- web/components/root/header.tsx
+++ web/components/root/header.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 import { RatingForm } from "../router/rating-form";
 import { Button, buttonVariants } from "../ui/button";
@@ -13,8 +13,6 @@ import { Star } from "lucide-react";
 import { RouteLinks } from "./utils/route-links";
 
 export const Header = () => {
-  const router = useRouter();
-
   const [isSignedIn, setIsSignedIn] = useState(false);
   const [modal, toggleModal] = useState(false);
 
@@ -33,28 +31,17 @@ export const Header = () => {
 
   const inSignInPage = usePathname() === "/router/overview";
 
-  const handleClick = () => {
-    router.refresh();
-    console.log("refresh");
-  };
-
   return (
     <header>
-      <nav className="fixed left-[50%] h-14 w-[40rem] -translate-x-1/2 gap-2 whitespace-nowrap border border-t-0 font-geistsans text-sm backdrop-blur">
+      <nav className="fixed left-[50%] h-14 w-[40rem] -translate-x-1/2 gap-2 whitespace-nowrap border border-t-0 font-geistsans backdrop-blur-md">
         <div className="absolute right-[100%] space-y-2 pr-8 pt-4">
-          <h1 className="text-2xl font-bold leading-6">jje</h1>
-          <div className="flex gap-[.1rem]">
-            {Array.from({ length: 5 }, (_, i) => (
-              <Star key={i} className="size-[1.125rem]" />
-            ))}
-          </div>
           <div className="flex flex-col gap-2">
             {!inSignInPage && (
               <Link
                 className={`${buttonVariants({ variant: "default" })} w-24`}
                 href={"/router/overview"}
               >
-                {isSignedIn ? "Overview" : "Sign in"}
+                {isSignedIn ? "Account" : "Sign in"}
               </Link>
             )}
             <Button
@@ -65,16 +52,16 @@ export const Header = () => {
               Rate!
             </Button>
             <Button
-              onClick={() => handleClick()}
+              onClick={() => toggleModal(!modal)}
               className="w-24"
               variant={"outline"}
             >
-              Refresh
+              Admin
             </Button>
           </div>
         </div>
 
-        <div className="absolute flex h-full w-full items-center justify-center gap-2">
+        <div className="absolute flex h-full w-full items-center justify-center gap-2 text-sm">
           <RouteLinks />
         </div>
       </nav>
