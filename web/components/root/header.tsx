18a6477d1aec639b90bc689a574bd1e177121067
ikeandrie <ikeandrie.ro@gmail.com>
2024-12-13 03:31:40 +0800

    fixed pathname check on overview & rate bttn on nav

--- web/components/root/header.tsx
+++ web/components/root/header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { buttonVariants } from "../ui/button";
+import { Button, buttonVariants } from "../ui/button";
 import Link from "next/link";
 import { RouteLinks } from "./utils/route-links";
 import { useEffect, useState } from "react";
@@ -22,13 +22,13 @@ export const Header = () => {
     checkUserSignIn();
   }, []);
 
-  const inSignInPage = usePathname() === "/route/overview";
+  const inSignInPage = usePathname() === "/router/overview";
 
   return (
     <header>
-      <nav className="fixed left-[50%] h-14 w-[40rem] -translate-x-1/2 gap-2 whitespace-nowrap border border-t-0 text-sm backdrop-blur">
+      <nav className="fixed left-[50%] h-14 w-[40rem] -translate-x-1/2 gap-2 whitespace-nowrap border border-t-0 font-geistsans text-sm backdrop-blur">
         <div className="absolute right-[100%] space-y-2 pr-8 pt-4">
-          <h1 className="font-geistsans text-2xl font-bold leading-6">jje</h1>
+          <h1 className="text-2xl font-bold leading-6">jje</h1>
           <div className="flex gap-[.1rem]">
             {Array.from({ length: 5 }, (_, i) => (
               <svg
@@ -48,14 +48,19 @@ export const Header = () => {
               </svg>
             ))}
           </div>
-          {!inSignInPage && (
-            <Link
-              className={`${buttonVariants({ variant: "default" })} w-22 font-geistsans`}
-              href={"/router/overview"}
-            >
-              {isSignedIn ? "Overview" : "Sign in"}
-            </Link>
-          )}
+          <div className="flex flex-col gap-2">
+            {!inSignInPage && (
+              <Link
+                className={`${buttonVariants({ variant: "default" })} w-24`}
+                href={"/router/overview"}
+              >
+                {isSignedIn ? "Overview" : "Sign in"}
+              </Link>
+            )}
+            <Button className="w-24" variant={"outline"}>
+              Rate!
+            </Button>
+          </div>
         </div>
         <div className="absolute flex h-full w-full items-center justify-center gap-2">
           <RouteLinks />
