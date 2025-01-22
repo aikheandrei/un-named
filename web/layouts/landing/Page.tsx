cd8f38fd8e7e1eaf41cace45fe66d9807870f979
aikhe <ikeandrie.ro@gmail.com>
2025-01-22 19:14:47 +0800

    docs(readme): changes

--- web/layouts/landing/Page.tsx
+++ web/layouts/landing/Page.tsx
@@ -1,7 +1,7 @@
 export default function Landing() {
   return (
     <section className="w-full font-[family-name:var(--font-geist-mono)] flex h-[100svh] items-center justify-center">
-      <div>magic era</div>
+      <div className="text-background">magic era</div>
     </section>
   );
 }
