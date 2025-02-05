6e8464c3440ea340a8060e136f2819a6b18291dc
aikhe <ikeandrie.ro@gmail.com>
2025-02-05 17:15:12 +0800

    refactor(auth): clean up auth formto their respective components

--- web/app/sign-in/page.tsx
+++ web/app/sign-in/page.tsx
@@ -1,37 +1,9 @@
-"use client";
-
-import { useState } from "react";
-import { usePathname, useRouter } from "next/navigation";
-
-import { login, signupWithOtp, signup } from "./actions";
-
-import OtpForm from "@/components/auth/otp-form";
+import AuthForm from "@/components/auth/auth-form";
 
 export default function SignInPage() {
-  const pathname = usePathname();
-  const router = useRouter();
-  const [isVerify, setIsVerify] = useState(false);
-
-  const handleSignin = (formData: FormData) => {
-    signupWithOtp(formData);
-    setIsVerify(!isVerify);
-
-    router.replace(pathname + "?email=" + formData.get("email"));
-  };
-
   return (
     <main>
-      <form>
-        <label htmlFor="email">Email:</label>
-        <input id="email" name="email" type="email" required />
-        <label htmlFor="password">Password:</label>
-        <input id="password" name="password" type="password" required />
-        <button formAction={login}>Log in</button>
-        <button formAction={signup}>Sign up</button>
-        <button formAction={handleSignin}>Send OTP</button>
-      </form>
-
-      {isVerify && <OtpForm />}
+      <AuthForm />
     </main>
   );
 }
