8c6395872cbebbf93041ab91d2f91c53d631f441
aikhe <ikeandrie.ro@gmail.com>
2025-02-09 10:28:18 +0800

    feat(auth): pass login api to try catch

--- web/app/api/auth/login/route.ts
+++ web/app/api/auth/login/route.ts
@@ -1,11 +1,21 @@
 import { AuthService } from "@/lib/auth/service";
 
 export async function POST(req: Request) {
-  const authService = new AuthService();
+  try {
+    const authService = new AuthService();
 
-  const { email, password } = await req.json();
+    const { email, password } = await req.json();
 
-  const { data } = await authService.login({ email, password });
+    const { data, error } = await authService.login({ email, password });
 
-  return Response.json(data);
+    if (error) {
+      return Response.json({ error }, { status: 400 });
+    }
+
+    return Response.json(data, { status: 200 });
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown error";
+    return Response.json({ error: errorMessage }, { status: 500 });
+  }
 }
