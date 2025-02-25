300dd768262cc47db5bbf24b0a95744f6cdf825e
aikhe <ikeandrie.ro@gmail.com>
2025-02-25 23:53:19 +0800

    refactor(auth): useSignOut, mannn i did made my life 100x harder sigh

--- web/app/(landing)/user/page.tsx
+++ web/app/(landing)/user/page.tsx
@@ -1,20 +1,20 @@
 import { redirect } from "next/navigation";
 
 import { createClient } from "@/lib/supabase/server";
-import { signOut } from "@/actions/auth/sign-out";
+import { useSignOut } from "@/hooks/useAuth";
 
 const UserPage = async () => {
   const supabase = await createClient();
 
   const { data, error } = await supabase.auth.getUser();
   if (error || !data?.user) {
-    redirect("/sign-up");
+    redirect("/sign-un");
   }
 
   return (
     <main>
       <p>Hello {data.user.email}</p>
-      <form action={signOut}>
+      <form action={useSignOut}>
         <button type="submit">Sign out</button>
       </form>
     </main>
