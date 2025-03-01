4db81d140937c8b5bf5bd801ff5d312a3922bb26
aikhe <ikeandrie.ro@gmail.com>
2025-03-01 23:07:11 +0800

    chore(auth): clean up user page & seperate user components

--- web/app/(landing)/user/page.tsx
+++ web/app/(landing)/user/page.tsx
@@ -1,37 +1,6 @@
-"use client";
-
-import { useEffect, useState, useTransition } from "react";
-import { redirect } from "next/navigation";
-import { User } from "@supabase/supabase-js";
-
-import { createBrowser } from "@/lib/supabase/client";
 import UserProfile from "@/components/User/UserProfile";
 
 const UserPage = () => {
-  const supabase = createBrowser();
-
-  const [signOutPending, startSignOut] = useTransition();
-  const [user, setUser] = useState<User>();
-
-  const signOut = () => {
-    startSignOut(async () => {
-      await supabase.auth.signOut();
-      redirect("/sign-up");
-    });
-  };
-
-  const fetchUser = async () => {
-    const { data, error } = await supabase.auth.getUser();
-    if (error || !data?.user) {
-      redirect("/sign-un");
-    }
-    setUser(data?.user);
-  };
-
-  useEffect(() => {
-    fetchUser();
-  }, []);
-
   return (
     <main>
       <UserProfile />
